2e70d98f83fb74fb254a6bec6302d5531e075c16
Jilshak <mohd.jilshak@gmail.com>
2023-09-22 19:16:56 +0530

    added a new model in the products

--- backend/products/models.py
+++ backend/products/models.py
@@ -13,6 +13,16 @@ class Categories(models.Model):
 def product_image_path(instance, filename):
     return os.path.join('product_images', str(instance.id), filename)
 
+
+class AddtionalInfo(models.Model):
+    ram = models.PositiveIntegerField(blank=True, null=True)
+    rom = models.PositiveBigIntegerField(blank=True, null=True)
+    size = models.CharField(max_length=100, blank=True, null=True)
+    color = models.CharField(max_length=100, blank=True, null=True)
+    
+    def __str__(self):
+        return self.ram
+
 class Product(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -22,9 +32,10 @@ class Product(models.Model):
     image4 = models.ImageField(blank=True, null=True, upload_to=product_image_path)
     price = models.PositiveIntegerField(blank=True, null=True)
     rating = models.PositiveIntegerField(blank=True, null=True)
+    additional_info = models.ForeignKey(AddtionalInfo, on_delete=models.CASCADE, blank=True, null=True)
     number_of_reviews = models.PositiveIntegerField(blank=True, null=True, default=0)
     count_in_stock = models.PositiveIntegerField(blank=True, null=True, default=5)
-    categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    categories = models.ForeignKey(Categories, on_delete=models.CASCADE, blank=True, null=True)
     seller = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
